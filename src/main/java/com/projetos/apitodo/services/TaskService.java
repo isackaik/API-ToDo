337d80b23dd5db7c040d543c23f1f2193d434bf0
Isac Kaik <isackaik1@gmail.com>
2023-06-24 09:09:05 -0300

    Create User and Task services

---
+++ src/main/java/com/projetos/apitodo/services/TaskService.java
@@ -0,0 +1,61 @@
+package com.projetos.apitodo.services;
+
+import com.projetos.apitodo.models.Task;
+import com.projetos.apitodo.models.User;
+import com.projetos.apitodo.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id){
+
+        Optional<Task> task = taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+                "Tarefa não encontrada. ID: " +id+ ", Tipo: " + Task.class.getName()
+        ));
+
+    }
+
+    @Transactional
+    public Task create(Task task){
+
+        User user = this.userService.findById(task.getUser().getId());
+        task.setId(null);
+        task.setUser(user);
+        task = this.taskRepository.save(task);
+
+        return task;
+    }
+
+    @Transactional
+    public Task update(Task task){
+
+        Task newTask = findById(task.getId());
+        newTask.setDescription(task.getDescription());
+
+        return this.taskRepository.save(newTask);
+
+    }
+
+    public void delete(Long id){
+
+        findById(id);
+
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Tarefa vinculada a outras entidades. Não foi possível excluir!");
+        }
+    }
+
+}
---
+++ src/main/java/com/projetos/apitodo/services/UserService.java
@@ -0,0 +1,57 @@
+package com.projetos.apitodo.services;
+
+import com.projetos.apitodo.models.User;
+import com.projetos.apitodo.repositories.TaskRepository;
+import com.projetos.apitodo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+           "Usuário não encontrado! Id: " +id+ ", Tipo: " + User.class.getName()
+        ));
+
+    }
+
+    @Transactional
+    public User create(User user){
+
+        user.setId(null);
+        user = this.userRepository.save(user);
+        return user;
+
+    }
+
+    @Transactional
+    public User update(User user){
+
+        User newUser = findById(user.getId());
+        newUser.setPassword(user.getPassword());
+        return this.userRepository.save(newUser);
+
+    }
+
+    public void delete(Long id){
+
+        findById(id);
+
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Usuário vinculado a outras entidades. Não foi possível excluir!");
+        }
+
+    }
+
+}
