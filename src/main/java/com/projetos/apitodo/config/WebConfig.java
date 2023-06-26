b919ea247bb5912945a57aa1e077d6404c2f8144
Isac Kaik <isackaik1@gmail.com>
2023-06-25 23:12:12 -0300

    Create FrontEnd with table of task

--- src/main/java/com/projetos/apitodo/config/WebConfig.java
+++ src/main/java/com/projetos/apitodo/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.projetos.apitodo.configs;
+package com.projetos.apitodo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/..");
+        registry.addMapping("/**");
 
     }
 
