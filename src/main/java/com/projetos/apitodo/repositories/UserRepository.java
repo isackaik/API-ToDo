51e36c492fd480e3991d28b97786214054334f25
Isac Kaik <isackaik1@gmail.com>
2023-07-01 15:04:50 -0300

    Create JWTUtil and UserDetailsServiceImpl

--- src/main/java/com/projetos/apitodo/repositories/UserRepository.java
+++ src/main/java/com/projetos/apitodo/repositories/UserRepository.java
@@ -3,8 +3,12 @@ package com.projetos.apitodo.repositories;
 import com.projetos.apitodo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 
 }
---
+++ src/main/java/com/projetos/apitodo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.projetos.apitodo.services;
+
+import com.projetos.apitodo.models.User;
+import com.projetos.apitodo.repositories.UserRepository;
+import com.projetos.apitodo.security.UserSpringSegurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if(Objects.isNull(user))
+            throw new UsernameNotFoundException("Usuário não encontrado: " + username);
+
+
+        return new UserSpringSegurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+}
